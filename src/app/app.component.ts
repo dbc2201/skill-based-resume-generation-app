c1fab31edc89cf86bbd5c0f6b8736c44d759866a
PRRANSHU787 <priyanshu001@gmail.com>
2024-03-17 20:58:19 +0530

    update some css tag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ContactFormComponent } from './components/contact-form/contact-form.component';
 import { SkillsFormComponent } from './components/skills-form/skills-form.component';
+import { FooterFormComponent } from "./components/footer-form/footer-form.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, ContactFormComponent, SkillsFormComponent, NavbarComponent]
+    imports: [RouterOutlet, ContactFormComponent, SkillsFormComponent, NavbarComponent, FooterFormComponent]
 })
 export class AppComponent {
   title = 'skill-based-resume-generation-app';
