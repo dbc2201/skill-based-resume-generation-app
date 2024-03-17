4b73b9c47dc9e59ed69eee2389f3782fef3348aa
PRRANSHU787 <priyanshu001@gmail.com>
2024-03-17 13:41:08 +0530

    create profile-form

---
+++ src/app/components/profile-form/profile-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './profile-form.component.html',
+  styleUrl: './profile-form.component.css'
+})
+export class ProfileFormComponent {
+
+}
