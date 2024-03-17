f357f541c99954559bc7f889946be37371b48251
PRRANSHU787 <priyanshu001@gmail.com>
2024-03-17 13:58:28 +0530

    create forgot-form component

---
+++ src/app/components/forgot-form/forgot-form.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ForgotFormComponent } from './forgot-form.component';
+
+describe('ForgotFormComponent', () => {
+  let component: ForgotFormComponent;
+  let fixture: ComponentFixture<ForgotFormComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ForgotFormComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(ForgotFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
