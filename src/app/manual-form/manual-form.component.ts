f93fe565f43182621ec06205f926b7e305fe72bd
PRRANSHU787 <priyanshu001@gmail.com>
2024-03-17 16:03:07 +0530

    create manual-from component

---
+++ src/app/manual-form/manual-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manual-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './manual-form.component.html',
+  styleUrl: './manual-form.component.css'
+})
+export class ManualFormComponent {
+
+}
