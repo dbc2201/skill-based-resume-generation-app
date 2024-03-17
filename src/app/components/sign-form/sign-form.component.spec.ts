5b057919bff8455ddb90008d183e119a8729be58
PRRANSHU787 <priyanshu001@gmail.com>
2024-03-17 13:57:49 +0530

    create sign-form component

---
+++ src/app/components/sign-form/sign-form.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SignFormComponent } from './sign-form.component';
+
+describe('SignFormComponent', () => {
+  let component: SignFormComponent;
+  let fixture: ComponentFixture<SignFormComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [SignFormComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(SignFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/sign-form/sign-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './sign-form.component.html',
+  styleUrl: './sign-form.component.css'
+})
+export class SignFormComponent {
+
+}
