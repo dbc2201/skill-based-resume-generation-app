1aed654de9cf86363597f1f6874b1aebb6b8af7c
PRRANSHU787 <priyanshu001@gmail.com>
2024-03-17 13:40:00 +0530

    create error-form

---
+++ src/app/components/error-form/error-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-error-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './error-form.component.html',
+  styleUrl: './error-form.component.css'
+})
+export class ErrorFormComponent {
+
+}
