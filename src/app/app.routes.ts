e8603f51c3535658f00d6b9fd038a62e00f22f05
PRRANSHU787 <priyanshu001@gmail.com>
2024-03-18 22:34:07 +0530

    create alert-form component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { ProfileFormComponent } from './components/profile-form/profile-form.component';
+import { ManualFormComponent } from './components/manual-form/manual-form.component';
+import { AssessmentFormComponent } from './components/assessment-form/assessment-form.component';
+import { ContactFormComponent } from './components/contact-form/contact-form.component';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+const routes: Routes = [
+	{path:'',component:AppComponent},
+	{path:'src\app\components\profile-form\profile-form.component.html',component:ProfileFormComponent },
+	{path:'src\app\components\manual-form',component:ManualFormComponent},
+	{path:'src\app\components\assessment-form',component:AssessmentFormComponent},
+	{path:'src\app\components\contact-form',component:ContactFormComponent},
+];
+@NgModule({
+imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class  AppRoutingModule {}
