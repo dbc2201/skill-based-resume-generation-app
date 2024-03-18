a50ccfa19ba1bd3c1c3968d38e7842441f2a7025
PRRANSHU787 <priyanshu001@gmail.com>
2024-03-18 22:40:27 +0530

    create popover component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,14 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { ContactFormComponent } from './components/contact-form/contact-form.component';
 import { SkillsFormComponent } from './components/skills-form/skills-form.component';
 import { FooterFormComponent } from "./components/footer-form/footer-form.component";
+import { AlertFormComponent } from "./components/alert-form/alert-form.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, ContactFormComponent, SkillsFormComponent, NavbarComponent, FooterFormComponent]
+    imports: [RouterOutlet, ContactFormComponent, SkillsFormComponent, NavbarComponent, FooterFormComponent, AlertFormComponent]
 })
 export class AppComponent {
   title = 'skill-based-resume-generation-app';
