ce2087e4dee195e6ff1e8e4f8fce4ec041b4e357
PRRANSHU787 <priyanshu001@gmail.com>
2024-03-18 09:11:33 +0530

    create demo-form component

---
+++ src/app/components/demo-form/demo-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-demo-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './demo-form.component.html',
+  styleUrl: './demo-form.component.css'
+})
+export class DemoFormComponent {
+
+}
