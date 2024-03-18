e05ffca9e3f03a83878d20f91903e468889735c3
PRRANSHU787 <priyanshu001@gmail.com>
2024-03-18 22:45:29 +0530

    edit html and css code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,13 +5,14 @@ import { ContactFormComponent } from './components/contact-form/contact-form.com
 import { SkillsFormComponent } from './components/skills-form/skills-form.component';
 import { FooterFormComponent } from "./components/footer-form/footer-form.component";
 import { AlertFormComponent } from "./components/alert-form/alert-form.component";
+import { PopoverFormComponent } from "./components/popover-form/popover-form.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, ContactFormComponent, SkillsFormComponent, NavbarComponent, FooterFormComponent, AlertFormComponent]
+    imports: [RouterOutlet, ContactFormComponent, SkillsFormComponent, NavbarComponent, FooterFormComponent, AlertFormComponent, PopoverFormComponent]
 })
 export class AppComponent {
   title = 'skill-based-resume-generation-app';
