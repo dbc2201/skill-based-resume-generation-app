c4503fc35dc0ea1d981dd567b7d890845396df1a
PRRANSHU787 <priyanshu001@gmail.com>
2024-03-25 22:47:22 +0530

    applying routing to component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ContactFormComponent } from './components/contact-form/contact-form.component';
 import { SkillsFormComponent } from './components/skills-form/skills-form.component';
@@ -14,7 +14,7 @@ import { DemoFormComponent } from "./components/demo-form/demo-form.component";
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, ContactFormComponent, SkillsFormComponent, NavbarComponent, FooterFormComponent, AlertFormComponent, PopoverFormComponent, ProfileFormComponent, DemoFormComponent]
+    imports: [RouterLink, ContactFormComponent, SkillsFormComponent, NavbarComponent, FooterFormComponent, AlertFormComponent, PopoverFormComponent, ProfileFormComponent, DemoFormComponent]
 })
 export class AppComponent {
   title = 'skill-based-resume-generation-app';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,15 +3,16 @@ import { ProfileFormComponent } from './components/profile-form/profile-form.com
 import { ManualFormComponent } from './components/manual-form/manual-form.component';
 import { AssessmentFormComponent } from './components/assessment-form/assessment-form.component';
 import { ContactFormComponent } from './components/contact-form/contact-form.component';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { AppComponent } from './app.component';
+import { SignFormComponent } from './components/sign-form/sign-form.component';
 
 export const routes: Routes = [
-	{path:'src\app\components',component:AppComponent},
-	{path:'src\app\components\profile-form\profile-form.component.html',component:ProfileFormComponent },
-	{path:'src\app\components\manual-form',component:ManualFormComponent},
-	{path:'src\app\components\assessment-form',component:AssessmentFormComponent},
-	{path:'src\app\components\contact-form',component:ContactFormComponent},
+	{path:'profile',component:ProfileFormComponent },
+	{path:'manual',component:ManualFormComponent},
+	{path:'assessment',component:AssessmentFormComponent},
+	{path:'contact',component:ContactFormComponent},
+	{path:'login',component:SignFormComponent},
 ];
 // @NgModule({
 // imports: [RouterModule.forRoot(routes)],
